ec326bc3e28139ffb6e5588537307a4865148dae
amon <amokim023@gmail.com>
2022-06-06 19:01:26 -0400

    Solutions for 0x00 hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints size of the various data types on the computer
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	char charType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n" sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n" sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n" sizeof(floatType);
+	return (0);
+}
