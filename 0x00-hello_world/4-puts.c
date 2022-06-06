f59f0f78fc328bff50c6a116e4f2f6dfdd936bff
amon <amokim023@gmail.com>
2022-06-06 18:17:40 -0400

    Solutions for 0x00 hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Prints output using puts function
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
